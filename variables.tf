714ec24617646a55566729297857e30011fc4ee3
Vicky99777 <93337057+Vicky99777@users.noreply.github.com>
2025-08-05 19:28:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,7 @@ variable "vikas" {
   description = "vikas bucket for portfolio hosting"
   default     = "vikas-bucket-aug5-1220pm" # must be globally unique!
 }
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
