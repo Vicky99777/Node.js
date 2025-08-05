322f58c07e78c73f0c55efc04ef345350cf8f0e4
Vicky99777 <93337057+Vicky99777@users.noreply.github.com>
2025-08-05 21:32:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
+variable "aws_region" {
+  description = "AWS region to create resources in"
+  type        = string
+  default     = "ap-south-1"  # Mumbai
+}
