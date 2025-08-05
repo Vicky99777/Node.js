47d1623afd0fc8827189f84e7e559f1b70433e12
Vicky99777 <93337057+Vicky99777@users.noreply.github.com>
2025-08-05 21:02:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,3 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-east-1"
-}
-
-variable "vikas" {
-  description = "vikas bucket for portfolio hosting"
-  default     = "vikas-bucket-aug5-1220pm" # must be globally unique!
-}
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
