130ef99565da7974ce7dc000b8b8cb0fb26ca92e
Vicky99777 <93337057+Vicky99777@users.noreply.github.com>
2025-08-05 21:43:15 +0530

    Update main.tf
    now adding index.html to try hosting our webpage .html

--- main.tf
+++ main.tf
@@ -1,8 +1,49 @@
+
 provider "aws" {
-  region = var.aws_region
+  region = "ap-south-1" # Mumbai
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "vikas-terraform-bucket-8a5e1c"
-  acl    = "private"
+  bucket = "vikas-portfolio-${random_id.bucket_suffix.hex}"
+  force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+
+  tags = {
+    Name = "Portfolio Hosting"
+  }
 }
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
